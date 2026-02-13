b359a31401d5bf93a72f170143ea4dc383fc05a6
TheCodeLamp <12064217+TheCodeLamp@users.noreply.github.com>
2026-02-13 10:48:44 +0100

    Laptop

--- build_scripts/install-laptop.sh
+++ build_scripts/install-laptop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 set -ouex pipefail
+
+export DRACUT_NO_XATTR=1
+dnf config-manager addrepo --assumeyes --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+dnf swap --assumeyes --allowerasing kernel-core kernel-surface
+dnf swap --assumeyes --allowerasing libwacom libwacom-surface
+dnf swap --assumeyes --allowerasing libwacom-data libwacom-surface-data
+dnf install --assumeyes iptsd
+dnf install --assumeyes surface-secureboot
