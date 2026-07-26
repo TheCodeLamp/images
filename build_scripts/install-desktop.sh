c393de2429e9ddcc4afed35cd22e8e6d728ba2cf
TheCodeLamp <12064217+TheCodeLamp@users.noreply.github.com>
2026-07-26 11:43:43 +0000

    Install `monado` (#28)

--- build_scripts/install-desktop.sh
+++ build_scripts/install-desktop.sh
@@ -11,6 +11,7 @@ solaar \
 wl-clipboard \
 glibc-devel.i686 libstdc++-devel.i686 vulkan-loader.i686 libX11.i686 \
 vulkan-tools vulkan-loader mesa-libGL \
+monado \
 
 dnf install --repo=firefoxpwa --assumeyes firefoxpwa
 
