32b78e9f8b2b9bdc2bffeda5c633a372398b6293
TheCodeLamp <12064217+TheCodeLamp@users.noreply.github.com>
2026-07-26 12:18:16 +0200

    remove false

--- build_scripts/install-desktop.sh
+++ build_scripts/install-desktop.sh
@@ -10,7 +10,7 @@ fira-code-fonts \
 solaar \
 wl-clipboard \
 glibc-devel.i686 libstdc++-devel.i686 vulkan-loader.i686 libX11.i686 \
-vulkan-tools vulkan-loader mesa-libvulkan mesa-libGL \
+vulkan-tools vulkan-loader mesa-libGL \
 
 dnf install --repo=firefoxpwa --assumeyes firefoxpwa
 
